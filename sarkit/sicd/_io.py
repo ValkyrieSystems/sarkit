7927d5ab0e14d1432a989f017cd593b612a8c199
mstewart-vsc <71520944+mstewart-vsc@users.noreply.github.com>
2025-07-14 08:50:39 -0700

    Support remote file reading (#50)
    * WIP
    
    * Add unit tests to verify ability to read remote files.
    
    * Add additional test dependency.
    
    * Formatting.
    
    * Add utility module for replicating np.fromfile functionality.
    
    * Apply 2 suggestion(s) to 2 file(s)
    
    Co-authored-by: Daniel Pressler <daniel.pressler@govsco.com>
    
    ---------
    
    Co-authored-by: Daniel Pressler <daniel.pressler@govsco.com>

--- sarkit/sicd/_io.py
+++ sarkit/sicd/_io.py
@@ -20,6 +20,7 @@ import numpy.typing as npt
 
 import sarkit.sicd._xml as sicd_xml
 import sarkit.wgs84
+from sarkit import _iohelp
 
 SPECIFICATION_IDENTIFIER: Final[str] = (
     "SICD Volume 1 Design & Implementation Description Document"
@@ -472,12 +473,12 @@ class NitfReader:
         imseg_sizes = np.asarray([imseg["Data"].size for imseg in imsegs])
         imseg_offsets = np.asarray([imseg["Data"].get_offset() for imseg in imsegs])
         splits = np.cumsum(imseg_sizes // (ncols * dtype.itemsize))[:-1]
-        for split, sz, offset in zip(
-            np.array_split(sicd_pixels, splits, axis=0), imseg_sizes, imseg_offsets
+        for split, offset in zip(
+            np.array_split(sicd_pixels, splits, axis=0), imseg_offsets
         ):
-            this_os = offset - self._file_object.tell()
-            split[...] = np.fromfile(
-                self._file_object, dtype, count=sz // dtype.itemsize, offset=this_os
+            self._file_object.seek(offset)
+            split[...] = _iohelp.fromfile(
+                self._file_object, dtype, np.prod(split.shape)
             ).reshape(split.shape)
         return sicd_pixels
 
