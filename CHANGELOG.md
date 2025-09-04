f9d2ae8841ab0f5b62e33692e49e601d446fa530
Daniel Pressler <daniel.pressler@govsco.com>
2025-09-04 14:31:09 +0000

    SICD sensitivity API tweaks in preparation for SICD v1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 - CLIs renamed from `<>-consistency` to `<>check`
 - Rearranged CRSD reference geometry API
+- Rearranged SICD sensitivity/error propagation API
 
 ### Removed
 - Several incomplete / NotImplemented APIs
--- tests/core/sicd/test_errorprop.py
+++ tests/core/sicd/test_errorprop.py
@@ -11,12 +11,8 @@ def test_compute_ric_basis_vectors():
 
 
 @pytest.mark.parametrize("frame", ("ECF", "RICF", "RICI"))
-def test_compute_ecef_pv_covariance(frame):
-    a = np.random.default_rng().random((6, 6))
-    c_pv = a @ a.T
-    c_pv_ecef = sicdproj.compute_ecef_pv_covariance(c_pv, [1, 2, 3], [4, 5, 6], frame)
+def test_compute_ecef_pv_transformation(frame):
+    t = sicdproj.compute_ecef_pv_transformation([1, 2, 3], [4, 5, 6], frame)
     if frame != "RICI":
-        assert np.linalg.eigvalsh(c_pv) == pytest.approx(np.linalg.eigvalsh(c_pv_ecef))
-    pos_eigvals = np.linalg.eigvalsh(c_pv[:3, :3])
-    pos_ecef_eigvals = np.linalg.eigvalsh(c_pv_ecef[:3, :3])
-    assert pos_eigvals == pytest.approx(pos_ecef_eigvals)
+        assert t @ t.T == pytest.approx(np.eye(6))
+    assert t[:3, :3] @ t[:3, :3].T == pytest.approx(np.eye(3))
